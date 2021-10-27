b9c5a8309626789702875ea1176c24114cc00c05
Bryan <bryanleng223@gmail.com>
2021-10-27 13:38:27 -0400

    -Added C major chords -Particles delete themselves after a certain time

--- Keyboard Garden Unity Project/Assets/Scripts/ChangeParticleColor.cs
+++ Keyboard Garden Unity Project/Assets/Scripts/ChangeParticleColor.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class ChangeParticleColor : MonoBehaviour
 {
+    public float timer = 1f;
+
     void Start()
     {
         ParticleSystem.MainModule psMain = GetComponent<ParticleSystem>().main;
         psMain.startColor = new ParticleSystem.MinMaxGradient(Color.white, Color.green);
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
