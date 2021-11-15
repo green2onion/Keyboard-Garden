90d2823c3f4134bbe417a363adfdcc08dcfd34a2
allichenn <allisonchen24@hotmail.com>
2021-11-15 11:39:01 -0500

    final update

--- Keyboard Garden Unity Project/Assets/Scripts/startButton.cs
+++ Keyboard Garden Unity Project/Assets/Scripts/startButton.cs
@@ -18,7 +18,7 @@ public class startButton : MonoBehaviour
     void Start()
     {
         audio = GetComponent<AudioSource>();
-        hover = true;
+        hover = false;
     }
 
     // Update is called once per frame
